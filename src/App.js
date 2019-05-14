e796375616362c6911cc2079f711b079f5868157
Jason Logan <jlogan@unitedfiregroup.com>
2019-05-14 15:50:16 -0500

    added ui-kit import

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from 'attackerator-ui-kit';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I pulled this button from the published example.
+        </p>
+        <Button>Whoa, a Button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
